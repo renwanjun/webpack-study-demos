734259b7717d88f3024a5909aa72a97cd9ab07e9
renwj <994435941@qq.com>
2020-12-03 20:20:07 +0800

    webpack处理css 和图片

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import './style.css'
+import Img from './new-year.jpg'
 
 function component() {
     var element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
+
+    var image=new Image()
+    image.src=Img
+
+    element.appendChild(image)
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
-const path=require("path")
+const path = require("path")
 
-module.exports={
-    entry:'./src/index.js',
-    output:{
-        filename:'bundle.js',
-        path:path.resolve(__dirname,'dist')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/.css$/,
-                use:['style-loader','css-loader']
+                test: /.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     }
