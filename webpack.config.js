df9fb67b3c8e1949a273534bd127cda38fd068ac
renwj <994435941@qq.com>
2020-12-07 20:32:36 +0800

    SplitChunksPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,18 @@
-const path = require("path")
-
+const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: {
-        index: {
-            import: './src/index.js',
-            dependOn: 'shared',
-        },
-        another: {
-            import: './src/another-module.js',
-            dependOn: 'shared',
-        },
-        shared: 'lodash',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    }
-}
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    another: './src/another-module.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+ optimization: {
+   splitChunks: {
+     chunks: 'all',
+   },
+ },
+};
