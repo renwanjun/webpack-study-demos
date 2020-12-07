a6b8df35fc85ed93195e4fd662812aab095cbf13
renwj <994435941@qq.com>
2020-12-07 20:49:12 +0800

    async函数改写

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 
 
-function getComponent() {
-    var element = document.createElement('div');
+// function getComponent() {
+//     var element = document.createElement('div');
 
-    return import('lodash').then(({default:_})=>{
-        const element=document.createElement('div')
-        element.innerHTML=_.join(['Hello','webpack'],'')
-        return element
-    }).catch(error=>'An error occurred while loading the component')
+//     return import('lodash').then(({default:_})=>{
+//         const element=document.createElement('div')
+//         element.innerHTML=_.join(['Hello','webpack'],'')
+//         return element
+//     }).catch(error=>'An error occurred while loading the component')
+// }
+
+// async await
+async funtion getComponent(){
+    var element = document.createElement('div');
+    const { default: _ } = await import('lodash')
+    const element = document.createElement('div')
+    element.innerHTML = _.join(['Hello', 'webpack'], '')
+    return element
 }
 
-getComponent.then(component=>{
+getComponent.then(component => {
     document.body.appendChild(component);
 })
