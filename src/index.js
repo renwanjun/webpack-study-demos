7f0afe482ea3ac61777c3221cb2ae9fe587ec79a
renwj <994435941@qq.com>
2020-12-07 20:41:12 +0800

    import()动态导入切割代码

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import _ from 'lodash'
 
-function component() {
+
+function getComponent() {
     var element = document.createElement('div');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
+    return import('lodash').then(({default:_})=>{
+        const element=document.createElement('div')
+        element.innerHTML=_.join(['Hello','webpack'],'')
+        return element
+    }).catch(error=>'An error occurred while loading the component')
 }
 
-document.body.appendChild(component());
+getComponent.then(component=>{
+    document.body.appendChild(component);
+})
