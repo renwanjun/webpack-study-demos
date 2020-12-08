a76e10147edf4c5070c28d66bb6cc75663965a68
renwj <994435941@qq.com>
2020-12-08 11:04:03 +0800

    删除dead-code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash'
+import { cube } from "./math";
 
 function component() {
-    var element = document.createElement('div');
+    var element = document.createElement('pre');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = ['Hello webpack!','5 cubed is equal to '+cube(5)].join('\n\n');
 
     return element;
 }
---
+++ src/math.js
@@ -0,0 +1,6 @@
+export function square(x){
+    return Math.pow(x,2)
+}
+export function cube(x){
+    return Math.pow(x,3)
+}
