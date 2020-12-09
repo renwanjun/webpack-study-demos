6b68268d0a0380462e9ef19fe5d58d0e7db0cbd5
renwj <994435941@qq.com>
2020-12-09 12:26:50 +0800

    按需加在

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 
+import _ from 'lodash'
 
-function getComponent() {
-    var element = document.createElement('div');
+function component() {
+    const element = document.createElement('div')
 
-    return import('lodash').then(({default:_})=>{
-        const element=document.createElement('div')
-        element.innerHTML=_.join(['Hello','webpack'],'')
-        return element
-    }).catch(error=>'An error occurred while loading the component')
+    const button = document.createElement('button')
+    const br = document.createElement('br')
+
+    button.innerHTML = 'Click me and look at the console!';
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    // Note that because a network request is involved, some indication
+    // of loading would need to be shown in a production-level site/app.
+    button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+        const print = module.default;
+
+        print();
+    });
+
+    return element;
 }
 
-// async await
-// async funtion getComponent(){
-//     var element = document.createElement('div');
-//     const { default: _ } = await import('lodash')
-//     const element = document.createElement('div')
-//     element.innerHTML = _.join(['Hello', 'webpack'], '')
-//     return element
-// }
-
-getComponent().then(component => {
-    document.body.appendChild(component);
-})
+document.body.appendChild(component())
