bc6474d570e5ad5f1b6e04980c8c35f86aa29a22
renwj <994435941@qq.com>
2020-12-09 12:35:13 +0800

    如果你遇到了至少一处用到 _ 变量的模块实例，那请你将 lodash package 引入进来，并将其提供给需要用到它的模块。

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path=require("path")
+const webpack=require("webpack")
+
 
 module.exports={
     entry:'./src/index.js',
     output:{
-        filename:'bundle.js',
+        filename:'main.js',
         path:path.resolve(__dirname,'dist')
-    }
+    },
+    plugins:[
+      new webpack.ProvidePlugin({
+        _:'lodash'
+      })
+    ]
 }
