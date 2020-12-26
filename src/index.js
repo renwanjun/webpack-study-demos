c70deb4b9c95215862381233da3f6ceaa7c14fe6
renwj <994435941@qq.com>
2020-12-26 16:03:36 +0800

    将第三方库(node_modules)的提取到单独的vendor chunk中

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function component() {
     const button = document.createElement('button')
     const br = document.createElement('br')
 
-    button.innerHTML = 'Click me and look at the console!';
+    button.innerHTML = 'Click2 me and look at the console!';
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.appendChild(br);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js'
+    main: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -14,7 +14,23 @@ module.exports = {
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization:
+  {
+    // moduleIds: 'deterministic',
+    // 提取引导代码 runtime.js
+    runtimeChunk:'single',
+    // 将第三方库(library)（例如 lodash 或 react）提取到单独的 vendor chunk 文件中
+    splitChunks:{
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    }
   }
 };
